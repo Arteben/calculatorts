f60880d0c773287d5a57af9cabb2052b29ee64b5
artem <artjombebenin@gmail.com>
2022-03-17 14:50:54 +0300

    set styles for the app

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,2 @@
 import '@/styles/style.scss'
-
-
-interface flashingObj {
-  first: string
-  second?: string
-}
-
-const setText = function(_someObj: flashingObj) {
-  const textElement = document.querySelector('#someText')
-  const text = _someObj.first
-  console.log(_someObj.second)
-  if (textElement && text && _someObj.second) {
-    textElement.innerHTML = text + _someObj.second
-  }
-}
-
-setText({first: 'a'})
+import * as app from '@/types/app'
---
+++ src/types/app.ts
@@ -0,0 +1,2 @@
+export type someType = number
+
